6d0455310284d01e102104d4726ca82ac1a868c1
amansachdev <46645259+amansachdev@users.noreply.github.com>
2021-01-27 10:57:54 +0530

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -21,7 +21,7 @@ from lxml import html
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from datetime import date, datetime, timedelta
-client = ScraperAPIClient('2078e23c0c1c054aa0e7ecffa3a81d49')
+client = ScraperAPIClient('fc82aac979ba378f7b4680c76019c2f2')
 #iplist = ['162.208.48.84:8118','165.138.4.41:8080']
 #proxies = {'https':random.choice(iplist) }
 #print(proxies)
